0c7864f8905d47d333a6879812437eed2bb2dfd0
Giovanna Marcondes <gmarcondes@dilettasolutions.com>
2024-02-17 17:34:27 -0300

    Created class and parse to get information of pokemóns

---
+++ tamagotchi/PokemonDetailsResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    internal class PokemonDetailsResult{
+        public List<AbilityDetail>? Abilities { get; set; }
+        public string? Name { get; set; }
+        public int Order { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public List<TypeDetail>? Types { get; set; }
+    }
+
+    public class AbilityDetail{
+        public Ability? Ability { get; set; }
+        public bool IsHidden { get; set; }
+        public int Slot { get; set; }
+    }
+
+    public class Ability{
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+    }
+
+    public class TypeDetail{
+        public Type? Type { get; set; }
+
+        public int Slot { get; set; }
+
+    }
+
+    public class Type{
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+    }
+}
